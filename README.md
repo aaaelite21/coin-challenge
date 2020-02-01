c9480f6cbe1421d3bdcf4365baa11254dbac018c
alexander ali <alexander_ali@my.uri.edu>
2020-02-01 09:18:36 -0500

    read me

--- README.md
+++ README.md
@@ -2,6 +2,14 @@
 
 Simple test to prove a statistical problem proposed at our daily lunch table.
 
+# The Problem
+
+You have 3 coins, on with two heads, one with two tales and one with both heads and tails. You then randomly select one of the coins. If you flip that coin twice in a row and get heads 3 times what is the percent chance of getting heads a third time. Assume all coins are fair.
+
+# The Result
+
+After being on the wrong side of this at the lunch table I decided I needed to try this out for myself. Given the choice between flipping coins all morning and writing a script I chose the latter. Something important to note head is that we are not choosing a new coin every time, meaning every flip gives us a little more information as to which coin, we have. Also, there are only a finite number of states within the scenario. After flipping 3 coins there are 8 possible combinations for each coin (each flip has two possible faces, 3 flips = 2^3) All eight of the possibilities for the heads only coin will yield heads on the 3rd flip. We remove all the possibilities given by the tails only coin because we know we do not have it. Finally, we remove all possibilities of the fair coin that include tails after two flips. This leaves us with two branches, one that goes heads-heads-tails and the other that goes heads-heads-heads. If we look at our group of remaining possible states, we will see that 9 of them yield heads-heads-heads and only one yields heads-heads-tails. This gives a 90% chance of flipping heads after flipping two previous heads given the rules of the game.
+
 # To Run
 
 - Download and install [NodeJS](https://nodejs.org/en/)
@@ -11,6 +19,8 @@ Simple test to prove a statistical problem proposed at our daily lunch table.
 - View Results
 
 ```
-Two Heads in A Row: 4176
-3rd head is flipped 90.254% of the time
+Two heads in a row was flipped 3650 times
+A 3rd head was flipped 89.863% of the time after that
 ```
+
+![meme](/wait_what.png)
--- coin-challenge.js
+++ coin-challenge.js
@@ -1,47 +1,50 @@
 const faces = {
-  heads: 0,
-  tails: 1
+    heads: 0,
+    tails: 1
 };
-class Coin {
-  constructor(face1, face2) {
-    this.faces = [face1, face2];
-  }
 
-  flip() {
-    return this.faces[Math.round(Math.random())];
-  }
+class Coin {
+    constructor(face1, face2) {
+        this.faces = [face1, face2];
+    }
+
+    flip() {
+        return this.faces[Math.round(Math.random())];
+    }
 }
 
-let i = 0;
 let coins = [
-  new Coin(faces.heads, faces.heads),
-  new Coin(faces.tails, faces.tails),
-  new Coin(faces.heads, faces.tails)
+    new Coin(faces.heads, faces.heads),
+    new Coin(faces.tails, faces.tails),
+    new Coin(faces.heads, faces.tails)
 ];
 
 let heads_after_two_heads = 0,
-  tails_after_two_heads = 0;
+    tails_after_two_heads = 0;
 
-while (i < 10000) {
-  let selectedCoin = coins[Math.floor(3 * Math.random())];
-  let flip1 = selectedCoin.flip();
-  let flip2 = selectedCoin.flip();
-  let flip3 = selectedCoin.flip();
+let power_level = 0;
 
-  if (flip1 === faces.heads && flip2 === faces.heads) {
-    if (flip3 === faces.heads) heads_after_two_heads++;
-    else tails_after_two_heads++;
-  }
-  i++;
+while (power_level < 9000) {
+    let selectedCoin = coins[Math.floor(3 * Math.random())];
+    let flip1 = selectedCoin.flip();
+    let flip2 = selectedCoin.flip();
+    let flip3 = selectedCoin.flip();
+
+    if (flip1 === faces.heads && flip2 === faces.heads) {
+        if (flip3 === faces.heads) heads_after_two_heads++;
+        else tails_after_two_heads++;
+    }
+
+    power_level++;
 }
 
 let percent_heads_after_two_heads =
-  (100 * heads_after_two_heads) /
-  (heads_after_two_heads + tails_after_two_heads);
+    (100 * heads_after_two_heads) /
+    (heads_after_two_heads + tails_after_two_heads);
 
 console.log(
-  `Two Heads in A Row: ${tails_after_two_heads + heads_after_two_heads}`
+    `Two heads in a row was flipped ${tails_after_two_heads + heads_after_two_heads} times`
 );
 console.log(
-  `3rd head is flipped ${percent_heads_after_two_heads.toFixed(3)}% of the time`
+    `A 3rd head was flipped ${percent_heads_after_two_heads.toFixed(3)}% of the time after that`
 );
